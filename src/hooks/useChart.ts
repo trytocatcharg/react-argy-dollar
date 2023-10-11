2f579557d249e8e6ddf291f64c31f91ea26974d6
trytocatch <blockchain@DESKTOP-C6LHPJ9.localdomain>
2023-10-11 08:35:13 +0200

    fix chart data

--- src/hooks/useChart.ts
+++ src/hooks/useChart.ts
@@ -13,19 +13,22 @@ export default function useChart () {
     useEffect(() => {
           fetch(url)
             .then(e => e.json())
-            .then(e => setDataChart(
-                            e.data[0].values.filter((_e, index) => {
-                              return (index === 0 || index % 5 == 0)
-                            }).map((i) => {
-                              const myDate = new Date(i[0]);
-                              const day = myDate.toLocaleString('default', { day: '2-digit' });
-                              const month = myDate.toLocaleString('default', { month: 'short' });
-                              return {
-                                date: day + ' ' + month,
-                                value: i[1]
-                              }
-                            })
-                        ));
+            .then(e => {
+                  const maxLength = e.data[0].values.length;
+                  setDataChart( e.data[0].values.filter((_e, index) => {
+                    const retrieveFirstLastAndFiveMultiple = index === 0 || (index % 5 == 0 && maxLength !== index) || (maxLength - 1) === index;
+                          return (retrieveFirstLastAndFiveMultiple)
+                              }).map((i) => {
+                                const myDate = new Date(i[0]);
+                                const day = myDate.toLocaleString('default', { day: '2-digit' });
+                                const month = myDate.toLocaleString('default', { month: 'short' });
+                                return {
+                                  date: day + ' ' + month,
+                                  value: i[1],
+                                }
+                              })
+                  )
+              })
         }, []);
 
 
